fede3f383e938215260b19ee33e8beff13a6184f
Rik Jonkmans <rik@tig.nl>
2018-06-20 16:18:18 +0200

    Fixed typo in namespace Mageplaza

--- Plugin/Mageplaza/AddressHelper.php
+++ Plugin/Mageplaza/AddressHelper.php
@@ -29,7 +29,7 @@
  * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
  * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  */
-namespace TIG\Postcode\Plugin\MagePlaza;
+namespace TIG\Postcode\Plugin\Mageplaza;
 
 use TIG\Postcode\Config\Provider\ModuleConfiguration;
 
