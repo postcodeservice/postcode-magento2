fae0f8be03d1c2611d2b2606f5f0e7354c7ecd6a
Rik Jonkmans <rik@tig.nl>
2018-06-21 13:28:21 +0200

    PSM2-50 - fix

--- Plugin/Mageplaza/AddressHelper.php
+++ Plugin/Mageplaza/AddressHelper.php
@@ -37,6 +37,8 @@ class AddressHelper
 {
     private $moduleConfig;
 
+    private $skipFields = ['firstname', 'lastname'];
+
     public function __construct(
         ModuleConfiguration $moduleConfiguration
     ) {
@@ -48,33 +50,54 @@ class AddressHelper
      * Compatible plugin, Mageplaza skippes grouped fields, so we need to add this manualy.
      *
      * @param         $subject
-     * @param         $fieldPostion
+     * @param         $fieldPosition
      *
      * @return mixed
      */
     // @codingStandardsIgnoreLine
-    public function afterGetAddressFieldPosition($subject, $fieldPostion)
+    public function afterGetAddressFieldPosition($subject, $fieldPosition)
     {
         if ($this->moduleConfig->isModusOff()) {
-            return $fieldPostion;
+            return $fieldPosition;
         }
 
-        return $this->addPostcodeFieldGroup($fieldPostion);
+        return $this->addPostcodeFieldGroup($fieldPosition);
     }
 
     /**
-     * @param $fieldPostion
+     * @param $fieldPosition
      *
      * @return mixed
      */
-    private function addPostcodeFieldGroup($fieldPostion)
+    private function addPostcodeFieldGroup($fieldPosition)
     {
-        $fieldPostion['postcode-field-group'] = [
-            'sortOrder' => 3,
+        $fieldPosition = $this->orderFields($fieldPosition);
+
+        $fieldPosition['postcode-field-group'] = [
+            'sortOrder' => 65,
             'colspan'   => 12,
             'isNewRow'  => true
         ];
 
-        return $fieldPostion;
+        return $fieldPosition;
+    }
+
+    /**
+     * @see \Mageplaza\Osc\Block\Checkout\LayoutProcessor::addAddressOption
+     * Because the postcode-field-group is not an Mageplaza OSC field it will be ignored.
+     * Affects since OSC version 2.5.0
+     *
+     * @param $fieldPosition
+     *
+     * @return mixed
+     */
+    private function orderFields(&$fieldPosition)
+    {
+        $sortOrder = 40;
+        foreach ($fieldPosition as $key => &$field) {
+            $field['sortOrder'] = $sortOrder += 10;
+        }
+
+        return $fieldPosition;
     }
 }
