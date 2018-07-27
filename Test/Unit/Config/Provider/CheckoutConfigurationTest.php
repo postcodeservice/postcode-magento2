4d3bfd4d0b5e2f1604dbe35559fc9c686de13fff
Rik Jonkmans <rik@tig.nl>
2018-07-27 12:39:40 +0200

    added config test

---
+++ Test/Unit/Config/Provider/CheckoutConfigurationTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\Postcode\Test\Unit\Config\Provider;
+
+use TIG\Postcode\Test\TestCase;
+use \TIG\Postcode\Config\Provider\CheckoutConfiguration;
+use \TIG\Postcode\Config\CheckoutConfiguration\GetCheckoutCompatibility;
+
+class CheckoutConfigurationTest extends TestCase
+{
+    protected $instanceClass = CheckoutConfiguration::class;
+
+    public function testGetConfig()
+    {
+        $checkoutCompatibleMock = $this->getFakeMock(GetCheckoutCompatibility::class)->disableOriginalConstructor()
+            ->getMock();
+        $checkoutCompatibleMock->expects($this->once())->method('getValue')->willReturn('this is a test');
+
+        $instance = $this->getInstance([
+            'postcodeConfiguration' => [
+                'test' => $checkoutCompatibleMock
+            ]
+        ]);
+
+        $result = [
+            'postcode' => [
+                'test' => 'this is a test'
+            ]
+        ];
+
+        $this->assertEquals($result, $instance->getConfig());
+    }
+}
