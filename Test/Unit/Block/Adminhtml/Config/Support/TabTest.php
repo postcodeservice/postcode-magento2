f507d37ce753f3b7733cfb7a025b82edf89b99be
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2020-10-12 16:07:23 +0200

    Updated to v1.3.0

--- Test/Unit/Block/Adminhtml/Config/Support/TabTest.php
+++ Test/Unit/Block/Adminhtml/Config/Support/TabTest.php
@@ -42,7 +42,7 @@ class TabTest extends TestCase
     public function testGetVersionNumber()
     {
         $instance = $this->getInstance();
-        $this->assertSame('1.2.7', $instance->getVersionNumber());
+        $this->assertSame('1.3.0', $instance->getVersionNumber());
     }
 
     public function testGetSupportedMagentoVersions()
