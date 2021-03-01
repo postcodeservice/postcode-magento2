ae14ab1d2e7046a196baca4e0265b696c7c4bab0
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2021-03-01 14:00:59 +0100

    Corrected supported Magento versions

--- Test/Unit/Block/Adminhtml/Config/Support/TabTest.php
+++ Test/Unit/Block/Adminhtml/Config/Support/TabTest.php
@@ -51,7 +51,7 @@ class TabTest extends TestCase
            'moduleConfiguration' => $this->getConfigurationMock()
         ]);
 
-        $this->assertSame('2.3.5 - 2.3.6-p1, 2.4.0 - 2.4.2', $instance->getSupportedMagentoVersions());
+        $this->assertSame('2.2.6 - 2.2.11, 2.3.0 - 2.3.6-p1, 2.4.0 - 2.4.2', $instance->getSupportedMagentoVersions());
     }
 
     /**+
@@ -62,7 +62,7 @@ class TabTest extends TestCase
         $mock = $this->getFakeMock(ModuleConfiguration::class)->getMock();
         $mockExpects = $mock->expects($this->once());
         $mockExpects->method('getSupportedMagentoVersions');
-        $mockExpects->willReturn('2.3.5 - 2.3.6-p1, 2.4.0 - 2.4.2');
+        $mockExpects->willReturn('2.2.6 - 2.2.11, 2.3.0 - 2.3.6-p1, 2.4.0 - 2.4.2');
 
         return $mock;
     }
