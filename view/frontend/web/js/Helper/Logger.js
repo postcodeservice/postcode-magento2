3c95ebe0a8c1c413b56b10f77dba09633f647b55
Kevin van Dijk <kevin.van.dijk@tig.nl>
2021-05-17 17:32:55 +0200

    POSTCODENL-263 update support email address in notice of license

--- view/frontend/web/js/Helper/Logger.js
+++ view/frontend/web/js/Helper/Logger.js
@@ -17,13 +17,13 @@
  * It is available through the world-wide-web at this URL:
  * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  * If you are unable to obtain it through the world-wide-web, please send an email
- * to support@postcodeservice.nl so we can send you a copy immediately.
+ * to support@postcodeservice.com so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade this module to newer
  * versions in the future. If you wish to customize this module for your
- * needs please contact support@postcodeservice.nl for more information.
+ * needs please contact support@postcodeservice.com for more information.
  *
  * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
  * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
